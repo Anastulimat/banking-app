b7f78830c009d4e74396efe9218324d307049e07
Anas TULIMAT <anas.tulimat@orange.com>
2024-07-10 15:56:38 +0200

    feat: add right side navbar with credit cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,43 @@
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 
 // ----------------------------------------------------------------------
 
 const Home = () => {
     const loggedIn = {
-        firstName: 'Anas'
+        firstName: 'Anas',
+        lastName: 'Tulimat',
+        email: 'anas.tulimat@gmail.com',
     }
 
     return (
         <section className="home">
             <div className="home-content">
-                <HeaderBox
-                    type="greeting"
-                    title="Welcome"
-                    user={loggedIn?.firstName || 'Guest'}
-                    subtext="Access and manage yout account and transactions efficiently"
-                />
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage yout account and transactions efficiently"
+                    />
 
-                <TotalBalanceBox
-                    accounts={[]}
-                    totalBanks={1}
-                    totalCurrentBalance={1250.35}
-                />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+
+                RECENT TRANS
             </div>
+
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{currentBalance: 123.50}, {currentBalance: 514.75}]}
+            />
+
         </section>
     );
 };
